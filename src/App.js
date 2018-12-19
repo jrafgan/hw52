ccf51b7505f8465cd659eb31ceb9128bde651cda
Maksat Duishenaliev <maksatovy@gmail.com>
2018-12-19 19:34:11 +0600

    Absent array code added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import React, {Component} from 'react';
 import Bingo from './components/bloc_div.js';
 import './App.css';
 
+let arr = [];
+let compareRandom = (a, b) => {
+    return a - b;
+};
 
+
+let rand = () => {
+    while (true) {
+
+        let a = Math.round(4.5 + Math.random() * 32);
+        let b = Math.round(4.5 + Math.random() * 32);
+        let c = Math.round(4.5 + Math.random() * 32);
+        let d = Math.round(4.5 + Math.random() * 32);
+        let e = Math.round(4.5 + Math.random() * 32);
+        if (a !== b && b !== c && c !== d && d !== a && b !== d && a !== c && e !== c && e !== a && e !== d) {
+            arr.push(a, b, c, d, e);
+            console.log(arr);
+            arr.sort(compareRandom);
+            break;
+        }
+
+
+    }
+};
+rand();
 
 class App extends Component {
     state = {
@@ -10,6 +34,7 @@ class App extends Component {
     };
 
     changeNumber = () => {
+        arr = [];
         rand();
         this.setState({number: arr});
     };
@@ -19,9 +44,9 @@ class App extends Component {
             <div className="App">
                 <div className="block">
                     <h1 className="h1">Bingo 5 of 36</h1>
-                    <div >
-                        {this.state.number.map ((num, key) => {
-                            return <Bingo key={key} num={this.state.number[key]} />
+                    <div>
+                        {this.state.number.map((num, key) => {
+                            return <Bingo key={key} num={this.state.number[key]}/>
                         })}
                     </div>
                     <button className="btn" onClick={this.changeNumber}>New Numbers</button>
